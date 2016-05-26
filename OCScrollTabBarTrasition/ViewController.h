1cc6b49d0fd3daf5b7267b6600caf23fcdd62676
Andrew <h20121028@126.com>
2016-05-26 22:45:47 +0800

    Initial Commit

---
+++ OCScrollTabBarTrasition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCScrollTabBarTrasition
+//
+//  Created by Andrew on 16/5/26.
+//  Copyright © 2016年 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCScrollTabBarTrasition/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OCScrollTabBarTrasition
+//
+//  Created by Andrew on 16/5/26.
+//  Copyright © 2016年 Andrew. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
